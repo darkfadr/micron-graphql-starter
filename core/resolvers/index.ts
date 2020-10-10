0016072e98b95e6051c726da3631fc77b56987da
Ashley Narcisse <ashlay49@gmail.com>
2020-10-10 00:59:26 -0400

    feat(merge): further reduced and simplified the implementation for the mergeResolvers helper

--- core/resolvers/index.ts
+++ core/resolvers/index.ts
@@ -1,4 +1,8 @@
 import { mergeResolvers } from './util';
 import * as User from './user';
+// import * as ExampleDomain from './example'
 
-export const resolvers = mergeResolvers({ User });
+export const resolvers = mergeResolvers(
+  User,
+  //ExampleDomain
+);
--- core/resolvers/util.ts
+++ core/resolvers/util.ts
@@ -1,25 +1,14 @@
 import { mergeResolvers as merge, IResolvers } from 'graphql-tools';
 
-interface IndexSignatureInterface {
-  [index: string]: any
+interface Resolver {
+  [index:string]: IResolvers
 }
 
-type StringIndexed<T> = T & IndexSignatureInterface;
+type MergedResolvers<T> = T & Resolver;
 
-export type Domains = {
-  [key:string]: IResolvers
+interface MergeResolvers {
+  (...resolvers: Resolver[]): MergedResolvers<IResolvers>
 }
 
-export const mergeResolvers = (domains: Domains): StringIndexed<IResolvers> => {
-  const resolvers = Object.keys(domains)
-    .map(key => {
-      const schema = domains[key];
-      //@ts-ignore | need a better way of setting the type level resolvers
-      schema[key] = schema.type;
-      delete schema.type;
-
-      return schema;
-    });
-
-  return merge(resolvers);
-};
+export const mergeResolvers: MergeResolvers = (...resolvers: Resolver[]) => 
+  merge(resolvers);
